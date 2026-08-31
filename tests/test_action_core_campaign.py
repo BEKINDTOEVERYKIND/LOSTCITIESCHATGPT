15206ec4b47a09b1d2ec2dc15cf5a139794a32a4
BEKINDTOEVERYKIND <63160015+BEKINDTOEVERYKIND@users.noreply.github.com>
2026-08-31 03:09:12 +0200

    Include policy-cost v12 in current source inventory

--- tests/test_action_core_campaign.py
+++ tests/test_action_core_campaign.py
@@ -147,6 +147,8 @@ class ActionCoreCampaignTests(unittest.TestCase):
                 "src/policy_cost_v10.h",
                 "src/policy_cost_v11.c",
                 "src/policy_cost_v11.h",
+                "src/policy_cost_v12.c",
+                "src/policy_cost_v12.h",
                 "src/history_belief_exclusion.c",
                 "src/history_belief_exclusion.h",
                 "src/history_belief_model.c",
