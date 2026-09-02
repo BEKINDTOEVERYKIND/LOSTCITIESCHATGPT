f466bf7de1ddd1bbb6c1a3c27a53dfcf76d4b0af
BEKINDTOEVERYKIND <63160015+BEKINDTOEVERYKIND@users.noreply.github.com>
2026-09-02 11:39:28 +0200

    Add hierarchical conditional-draw v17 definition

--- tests/test_action_core_campaign.py
+++ tests/test_action_core_campaign.py
@@ -158,6 +158,8 @@ class ActionCoreCampaignTests(unittest.TestCase):
                 "src/policy_cost_v15.h",
                 "src/policy_cost_v16.c",
                 "src/policy_cost_v16.h",
+                "src/policy_cost_v17.c",
+                "src/policy_cost_v17.h",
                 "src/history_belief_exclusion.c",
                 "src/history_belief_exclusion.h",
                 "src/history_belief_model.c",
